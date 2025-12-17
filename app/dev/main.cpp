6cb11a9e35d1fc6c7295ea14bf9413e12958d202
LiveLongFlame <valentinoosorioschwarz@gamil.com>
2025-12-17 14:37:23 +1100

    added structure and folders for git project and further got basic template and test of using mlpack

---
+++ app/dev/main.cpp
@@ -0,0 +1,11 @@
+#include <mlpack.hpp>
+#include <mlpack/core/util/version.hpp>
+
+using namespace std;
+int main(){
+ // printing version of mlpack and returing it as a string
+ 
+	cout << "mlpack version: " << mlpack::util::GetVersion() << "\n";
+	return 0;
+	
+}
