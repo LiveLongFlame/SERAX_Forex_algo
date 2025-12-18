c928b14df32e74ddd52f63a81c4b9816da8551cf
LiveLongFlame <valentinoosorioschwarz@gamil.com>
2025-12-18 21:44:42 +1100

    added todos and plan for how to extract data into chunks then adding those chunks into one large csv file

--- src/dev/data/code/extracting_data.py
+++ src/dev/data/code/extracting_data.py
@@ -1,15 +1,27 @@
-# This file is for connecting to the ibkr api and extracting historical data in order to train ML aglo
+# Program will go throuhg each currency pair and request a chunk of the data each time and store it as a csv file in a specified folder.
+# The reason we do this is because IBKR does not allow big certain amount of request data. Therefore, we will break the data into different chuncks then run another program which will allow us to combine the chunk data.
 from ib_insync import *
-# util.startLoop()  # uncomment this line when in a notebook
+import pandas as pd
+import datatime
+import os
+import time
 
 ib = IB()
 ib.connect('127.0.0.1', 4002, clientId=1)
 
-contract = Forex('EURAUD')
-bars = ib.reqHistoricalData(
-    contract, endDateTime='', durationStr='6 M',
-    barSizeSetting='1 hour', whatToShow='MIDPOINT', useRTH=True)
+#Differnt currency pairs
+pairs = [
+        ('EUR','USD'),
+        ('USD','JPY'),
+        ('GBP','USD'),
+        ('USD','CHF'),
+        ('USD','CAD'),
+        ('AUD','USD'),
+        ('NZD','USD'),
+        ]
 
-# convert to pandas dataframe (pandas needs to be installed):
-df = util.df(bars)
-print(df)
+# todo: add different parameteres in order to define what data needs to be extracted. Example: 5 Y with bar size of 1min with 2 day chunks....
+
+#todo: save data in csv inside folder 
+
+# call api and extract the information.
