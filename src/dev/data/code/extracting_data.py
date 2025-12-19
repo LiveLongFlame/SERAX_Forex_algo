bf981b033a740a57fd64c004b222e4af6b4175b6
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2025-12-19 22:12:44 +1100

    added base os to where the data will be stored and todo list

--- src/dev/data/code/extracting_data.py
+++ src/dev/data/code/extracting_data.py
@@ -1,13 +1,18 @@
 # Program will go throuhg each currency pair and request a chunk of the data each time and store it as a csv file in a specified folder.
 # The reason we do this is because IBKR does not allow big certain amount of request data. Therefore, we will break the data into different chuncks then run another program which will allow us to combine the chunk data.
+from datetime import datetime
 from ib_insync import *
 import pandas as pd
 import datatime
 import os
 import time
 
+# Connects to ib gateway software and information
 ib = IB()
 ib.connect('127.0.0.1', 4002, clientId=1)
+
+# create base directory 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 #Differnt currency pairs
 pairs = [
@@ -20,8 +25,19 @@ pairs = [
         ('NZD','USD'),
         ]
 
-# todo: add different parameteres in order to define what data needs to be extracted. Example: 5 Y with bar size of 1min with 2 day chunks....
+# Set parameters that we want to extract information
+end_date = datetime.datetime.now()
+start_date = end_date - datetime.timedelta(days=5*365)
+delta = datetime.timedelta(days=2)
+bar_size = '1min'
 
-#todo: save data in csv inside folder 
+# Save the data into the data folder
+save_folder = os.path.join(BASE_DIR, 'data')
+os.makedirs(save_folder, exist_ok=True)
 
-# call api and extract the information.
+#Function to fetch historical data in chunks
+def fetch_and_save(contract_name, contract):
+    #todo: connect to gateway with parameters 
+    #todo: create loop the itrates through the data and information allowing extract information
+    #todo: move informationa and save things into a csv file 
+    return 0;
