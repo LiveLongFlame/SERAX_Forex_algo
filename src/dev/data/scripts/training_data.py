4ee9d42cd272257c87f0c454ce8845769fde70f0
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2026-02-05 22:33:13 +1100

    changed python to fit path

--- src/dev/data/scripts/training_data.py
+++ src/dev/data/scripts/training_data.py
@@ -8,11 +8,12 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 
 DATA_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, ".."))
 COMBINED_FILE = os.path.join(DATA_DIR, "combined.csv")
+OHLC_CSV = os.path.join(DATA_DIR, "ohlc.csv")
 
 df = pd.read_csv(COMBINED_FILE)
 
 # keep only the columns you care about
 ohlc = df[["open", "high", "low", "close"]]
 
-ohlc.to_csv("ohlc.csv", index=False)
+ohlc.to_csv(OHLC_CSV, index=False)
 
