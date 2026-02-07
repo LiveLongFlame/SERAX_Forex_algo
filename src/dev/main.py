b547120df7dfb794d1dee2cb504734d643a5a15a
LiveLongFlame <valentinoosorioschwarz@gmail.com>
2026-02-08 00:31:41 +1100

    added functionality to connect to ibkr paper trading and changd roc and sdor in order to view information

--- src/dev/main.py
+++ src/dev/main.py
@@ -7,13 +7,42 @@ import pandas as pd
 import numpy as np
 #todo: import ML model here
 
-#todo: create function that gets live data from the last hour and feeds it into sdor and roc 
-#todo: after calcualting sdor and roc, feed those values into the ML model to make predictions
-#todo: each trade should be based on the predicted values from the ML model and the current market conditions 
 #todo: print out users current balance, open positions, and any other relevant information to the console for each live trade
 #todo: users should be alowwed to hit ctrl+c to exit the application at any time and the program should print out a summary of the users trading performance for the session before exiting
-def sdor(roc_vals: np.ndarray):
-    return np.std(roc_vals, ddof=0) 
+
+pairs = ['EURUSD', 'USDJPY' , 'GDPUSD', 'USDCHF', 'USDCAD', 'AUDUSD', 'NZDUSD']
+
+def ml_model(ccy_pair ib, model):
+    contract = Forex(ccy_pair)
+    ib.qualifyContracts(contract)
+    bars = ib.reqHistoricalData(
+            contract,
+            endDateTime='',
+            durationStr='2 H',
+            barSizeSetting='1 min',
+            whatToShow='MIDPOINT',
+            useRTH=False,
+            formatDate=1
+            )
+    df = util.df(bars)
+    df.set_index('date', inplace=True)
+
+    if not bars:
+        print("No data received for the specified currency pair.")
+        return 1 
+    
+    #todo: implement model with pybind 11
+    return 0
+
+#todo: this function should execute a trade based on the predicted values from the ML model and the current market conditions
+def trade():
+    return 0
+
+def sdor(returns: np.ndarray):
+    downside = returns[returns < 0]
+    if len(downside) == 0:
+        return 0.0
+    return np.sqrt(np.mean(downside ** 2))
 
 def roc(close_prices: np.ndarray):
     return np.diff(close_prices) / close_prices[:-1]
@@ -37,7 +66,7 @@ def menu():
     elif choice == 2:
         initial = float(input("Enter initial value $ "))
         #FOR TESTING PURPOSES ONLY
-        print("Initial value: $", initial)
+        print("Initial value: ", initial)
         #------------------------------------
         #todo: add code to use ML model to make predictions here
     elif choice == 3:
@@ -50,9 +79,9 @@ def menu():
 def main():
     print("Starting application...")
     print("Initializing IB connection...")
-    # ib = IB()
+    ib = IB()
     #change port denpending on your IB Gateway or TWS settings
-    # ib.connect('127.0.0', 4002, clientId=1)
+    ib.connect('127.0.0', 4002, clientId=1)
     print("IB connection established...")
 
     #todo: get live paper trail data 
